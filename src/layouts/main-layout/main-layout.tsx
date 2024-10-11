d53685c1a29ac9a54d782c3307abc0e38e1a7265
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-11 20:35:14 +0330

    main layout created

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -4,7 +4,7 @@ import {Footer} from "../../components/footer/footer.tsx";
 
 export const MainLayout = () => {
     return (
-        <div className='grid grid-rows-[80px_1fr_auto] min-h-screen bg-gray-200 text-gray-600'>
+        <div className='grid grid-rows-[80px_1fr_auto] min-h-screen bg-gray-900 text-gray-600'>
             <Header/>
                 <main>
                     <Outlet/>
