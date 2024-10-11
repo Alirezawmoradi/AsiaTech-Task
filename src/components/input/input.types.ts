80e712f0adb5d083d9ac157661a02db43034cb95
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-11 20:50:32 +0330

    input types created

---
+++ src/components/input/input.types.ts
@@ -0,0 +1,10 @@
+import {ChangeEvent, KeyboardEvent, RefObject} from "react";
+
+export interface InputProps {
+    value: string;
+    placeholder?: string;
+    inputRef: RefObject<HTMLDivElement>;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    onKeyDown: (e: KeyboardEvent<HTMLInputElement>) => void;
+    onSearchIconClick: () => void;
+}
