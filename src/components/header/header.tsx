3a794e7033104a70582aaba5b50a36d352618ae3
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-11 20:34:24 +0330

    header component created

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,9 +2,9 @@ import * as React from "react";
 
 export const Header: React.FC = () => {
     return (
-        <header className="border-b border-gray-300 border-opacity-5 flex bg-gray-300">
+        <header className="border-b border-gray-300 border-opacity-5 flex">
             <div className="container xl:flex items-center justify-center">
-                <h1 className='xl:text-3xl md:text-2xl font-bold text-gray-500'>AsiaTech Frontend Task</h1>
+                <h1 className='xl:text-3xl md:text-2xl font-bold text-gray-300'>AsiaTech Frontend Task</h1>
             </div>
         </header>
     )
