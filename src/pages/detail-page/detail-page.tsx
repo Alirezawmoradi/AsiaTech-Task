29fcde0826cf58caba614e9976a1c888236f6897
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-11 20:34:05 +0330

    details page created

--- src/pages/detail-page/detail-page.tsx
+++ src/pages/detail-page/detail-page.tsx
@@ -1,7 +1,9 @@
+import {Results} from "../../components/results";
 
 export const DetailPage: React.FC = () => {
     return (
         <div className='flex flex-col gap-10 justify-center items-center text-xl h-full'>
+            <Results/>
         </div>
     );
 };
