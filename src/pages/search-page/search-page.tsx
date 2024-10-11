46b686eddb4d9043e49b4c1f535bcbb3a72b3b32
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-11 20:35:25 +0330

    search page created

--- src/pages/search-page/search-page.tsx
+++ src/pages/search-page/search-page.tsx
@@ -1,7 +1,15 @@
+import {Search} from "../../components/search";
+
 export const SearchPage: React.FC = () => {
 
     return (
         <div className='flex flex-col gap-10 justify-center items-center text-xl h-full'>
+            <div className='gap-5 justify-center items-center'>
+                <img src='../../../public/images/logo.png' alt='Asiatech' className='w-full h-full'/>
+            </div>
+            <div className='relative w-10/12'>
+                <Search/>
+            </div>
         </div>
     );
 };
