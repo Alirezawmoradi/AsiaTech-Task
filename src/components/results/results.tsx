b6a0d4c184560d6947a20341219a3cdaa59da946
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-11 20:40:48 +0330

    results component created

---
+++ src/components/results/results.tsx
@@ -0,0 +1,38 @@
+import {useSearchStore} from "../../stores/search-store/useSearchStore.ts";
+import {useEffect} from "react";
+import {Loading} from "../loading";
+
+export const Results = () => {
+    const {query, loading, results} = useSearchStore();
+    const {getResults} = useSearchStore((state) => state.actions);
+
+    useEffect(() => {
+        if (query !== '') {
+            getResults(`q=${query}&num=40`)
+        }
+    }, [query]);
+    return (
+        <div className="sm:px-56 flex flex-col justify-between space-y-6">
+            {loading ?
+                <Loading/>
+                :
+                <>
+                    <div className='flex flex-wrap gap-10'>
+                        {Array.isArray(results) && results.length > 0 ? (
+                            results.map(({url, title}, position) => (
+                                <div key={position} className="md:w-2/5 w-full">
+                                    <a href={url} target="_blank" rel="noreferrer">
+                                        <p className="text-lg hover:underline text-blue-400">{title}</p>
+                                        <p className="text-sm text-gray-400">{url.length > 30 ? url.substring(0, 30) : url}</p>
+                                    </a>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No results found.</p>
+                        )}
+                    </div>
+                </>
+            }
+        </div>
+    )
+}
