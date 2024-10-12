21026e97ed04dbc3ca038869273d5a8e5248f907
Alirezaw <Alirezawmoradi@gmail.com>
2024-10-12 03:47:52 +0330

    responsive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 
 export const Header: React.FC = () => {
     return (
-        <header className="border-b border-gray-300 border-opacity-5 flex">
-            <div className="container xl:flex items-center justify-center">
+        <header className="border-b border-gray-300 border-opacity-5 flex justify-center items-center">
+            <div className="container xl:flex items-center justify-center text-center">
                 <h1 className='xl:text-3xl md:text-2xl font-bold text-gray-300'>AsiaTech Frontend Task</h1>
             </div>
         </header>
